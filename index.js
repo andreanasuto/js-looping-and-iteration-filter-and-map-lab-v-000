9a691fbf19fe62b90f82b0d324a079bb3500627e
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 18:56:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function driversWithRevenueOver(array, revenue) {
   array.filter(function filterfunction(element) {
     element.revenue >= revenue
   })
+  console.log(filteredarray)
   return filteredarray
 }
