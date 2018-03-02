aece668852ad588f3b8bad31e1264212141eb04e
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 21:11:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function driverNamesWithRevenueOver(array,revenue) {
 
 function exactMatch(array, value) {
   const match = array.map(function searchMatch(element) {
+    console.log(value.name);
       if (element.name == value.name) {
         return Object.assign({}, {name: element.name}, {revenue: element.revenue })
       }
