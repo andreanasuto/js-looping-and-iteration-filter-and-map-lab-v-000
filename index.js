f4a8f357195e805f12ed3a60c5134e4a9d39106b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 18:57:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here:
 function driversWithRevenueOver(array, revenue) {
   const filteredarray =
-  array.map(function filterfunction(element) {
+  array.filter(function filterfunction(element) {
     console.log(element.revenue)
     element.revenue >= revenue
   })
